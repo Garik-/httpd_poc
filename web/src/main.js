f7008b6f8a11708fd72f018d818d343e88019dab
Gar|k <garik.djan@gmail.com>
2026-01-11 20:28:01 +0300

    feat: add some web design

--- web/src/main.js
+++ web/src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const ledButton = document.getElementById('led_button');
+let ledEnable = false; // Initial state can be fetched from the server if needed
+ledButton.addEventListener('click', async () => {
+    
+  try {
+    const response = await fetch(ledEnable ? '/api/led/off' : '/api/led/on', { method: 'POST' });
+    if (response.ok) {
+      ledEnable = !ledEnable;
+      ledButton.classList.toggle('fill');
+    } else {
+      console.error(response.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
